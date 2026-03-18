f8eafbb862fce5129dc66456503873fe6c803b88
ayush <100313112+12catsk@users.noreply.github.com>
2026-03-17 22:11:37 -0600

    updates css and contact info

--- app/page.js
+++ app/page.js
@@ -1,19 +1,24 @@
+import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main style={{ maxWidth: "860px", margin: "0 auto", padding: "0 2rem" }}>
-      <Hero />
-      <hr style={{ border: "none", borderTop: "0.5px solid var(--border)" }} />
-      <About />
-      <hr style={{ border: "none", borderTop: "0.5px solid var(--border)" }} />
-      <Projects />
-      <hr style={{ border: "none", borderTop: "0.5px solid var(--border)" }} />
-      <div style={{ maxWidth: "1100px", margin: "0 auto", padding: "0 4rem" }}></div>
-      <Contact />
+    <main>
+      <Navbar />
+      <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "0 2rem" }}>
+        <Hero />
+        <hr style={{ border: "none", borderTop: "0.5px solid var(--border)" }} />
+        <About />
+        <hr style={{ border: "none", borderTop: "0.5px solid var(--border)" }} />
+        <Projects />
+        <hr style={{ border: "none", borderTop: "0.5px solid var(--border)" }} />
+        <Contact />
+      </div>
+      <Footer />
     </main>
   );
 }
